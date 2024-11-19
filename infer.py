fc78d69ef01b26489dd3668732b72b893729ed25
lsyzz <961341853@qq.com>
2024-11-19 11:55:11 +0800

    f

--- infer.py
+++ infer.py
@@ -6,6 +6,6 @@ from ultralytics import YOLO
 model = YOLO("single/train/weights/best.pt")
 
 # from PIL
-im1 = Image.open("P0003__682__0___0.jpg")
+im1 = Image.open("/home/zeyang/code/datasets/DOTAv1-split_500/images/test/P0006__1024__0___0.jpg")
 results = model.predict(source=im1, save=True,show_labels=False,show_conf=False)  # save plotted images
 
